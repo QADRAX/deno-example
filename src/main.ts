980d1dbf0ed252283fd9850da522e4db2e96c320
Carlos de la Cuadra <carlos_qadra@hotmail.com>
2022-02-12 14:36:47 +0100

    chore: debugging for vscode

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { serve } from "https://deno.land/std@0.95.0/http/server.ts";
+import { serve } from "../deps.ts";
 
 const server = serve({ port: 8000 });
-console.log("http://localhost:8000/");
+
+console.log("hosting app in http://localhost:8000/");
 
 for await (const req of server) {
-  req.respond({ body: "KLwass \n" });
+  req.respond({ body: "KLK \n" });
 }
